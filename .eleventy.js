61c2d13a167f0f6deee7c6c5bdbe783ebe0d73af
matianfu <matianfu@gingerologist.com>
2024-02-12 21:11:12 +0800

    update .eleventy.js according to crash course

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,13 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: 'src',
+      includes: '_includes',
       output: 'dist',
-    }
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
   }
 }
 
